6fb6dc8022bfedd0caf57d89e4b0aec9ed0c3e4e
Jorge Rivera <jorge.rivera@one.org>
2023-07-17 11:15:20 +0200

    add new channel strategy

--- climate_finance/config.py
+++ climate_finance/config.py
@@ -8,6 +8,7 @@ class ClimateDataPath:
     project = Path(__file__).resolve().parent.parent
     scripts = project / "climate_finance"
     raw_data = scripts / ".raw_data"
+    unfccc_cleaning_tools = scripts / "unfccc" / "cleaning_tools"
 
 
 # Create a root logger
