85801c93d828aa9697eba3415e2a1059f4732429
Jorge Rivera <jorge.rivera@one.org>
2023-10-23 22:34:42 +0200

    names functions

--- climate_finance/oecd/cleaning_tools/names.py
+++ climate_finance/oecd/cleaning_tools/names.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from oda_data import read_crs
 
+from climate_finance.config import ClimateDataPath
 from climate_finance.oecd.cleaning_tools.schema import CrsSchema
 from climate_finance.oecd.cleaning_tools.tools import rename_crs_columns, idx_to_str
 from climate_finance.oecd.climate_related_activities.recipient_perspective import (
@@ -8,40 +9,173 @@ from climate_finance.oecd.climate_related_activities.recipient_perspective impor
 )
 
 
-def add_provider_agency_names(data: pd.DataFrame, crs_year: int = 2021) -> pd.DataFrame:
+def _create_names(
+    crs_year: int, crs_idx: list[str], merge_idx: list[str], file_name: str
+) -> None:
+    # Get the CRS data for the year specified
+    crs = (
+        read_crs([crs_year])  # read the CRS
+        .pipe(rename_crs_columns)  # rename the columns
+        .drop_duplicates(subset=crs_idx)  # drop duplicates by index
+        .filter(items=crs_idx)  # keep only the columns in the index
+        .pipe(idx_to_str, idx=crs_idx)  # convert the index to strings
+    )
+
+    # Get the CRDF data for the year specified
+    crdf = (
+        get_recipient_perspective(start_year=crs_year, end_year=crs_year)
+        .filter(items=crs_idx)  # keep only the columns in the index
+        .drop_duplicates(subset=crs_idx)  # drop duplicates by index
+        .pipe(idx_to_str, idx=merge_idx)
+    )
+
+    # Join the two datasets with different naming conventions
+    crs = (
+        pd.concat([crs, crdf], ignore_index=True)
+        .drop_duplicates(subset=merge_idx)
+        .reset_index(drop=True)
+    )
+
+    # Save the data
+    crs.to_feather(
+        ClimateDataPath.scripts / "oecd" / "cleaning_tools" / f"{file_name}.feather"
+    )
+
+
+def create_provider_agency_names(crs_year: int = 2021) -> None:
+    # Define the index that will be used to identify unique names
     idx = [
         CrsSchema.PARTY_CODE,
         CrsSchema.AGENCY_CODE,
         CrsSchema.PARTY_NAME,
         CrsSchema.AGENCY_NAME,
     ]
-    crs = (
-        read_crs([crs_year])
-        .pipe(rename_crs_columns)
-        .drop_duplicates(subset=idx)
-        .filter(items=idx)
-        .pipe(idx_to_str, idx=idx)
+
+    merge_idx = [CrsSchema.PARTY_CODE, CrsSchema.AGENCY_CODE]
+
+    _create_names(
+        crs_year=crs_year,
+        crs_idx=idx,
+        merge_idx=merge_idx,
+        file_name="provider_agency_names",
     )
 
-    crdf = (
-        get_recipient_perspective(start_year=crs_year, end_year=crs_year)
-        .filter(items=idx)
-        .drop_duplicates(subset=idx)
-        .pipe(idx_to_str, idx=[CrsSchema.PARTY_CODE, CrsSchema.AGENCY_CODE])
+
+def create_provider_names(crs_year: int = 2021) -> None:
+    # Define the index that will be used to identify unique names
+    idx = [CrsSchema.PARTY_CODE, CrsSchema.PARTY_NAME]
+
+    merge_idx = [CrsSchema.PARTY_CODE]
+
+    _create_names(
+        crs_year=crs_year,
+        crs_idx=idx,
+        merge_idx=merge_idx,
+        file_name="provider_names",
     )
 
-    crs = pd.concat([crs, crdf], ignore_index=True).drop_duplicates(
-        subset=[CrsSchema.PARTY_CODE, CrsSchema.AGENCY_CODE]
+
+def create_recipient_names(crs_year: int = 2021) -> None:
+    # Define the index that will be used to identify unique names
+    idx = [CrsSchema.RECIPIENT_CODE, CrsSchema.RECIPIENT_NAME]
+
+    merge_idx = [CrsSchema.RECIPIENT_CODE]
+
+    _create_names(
+        crs_year=crs_year,
+        crs_idx=idx,
+        merge_idx=merge_idx,
+        file_name="recipient_names",
     )
 
+
+def read_provider_agency_names() -> pd.DataFrame:
+    return pd.read_feather(
+        ClimateDataPath.scripts
+        / "oecd"
+        / "cleaning_tools"
+        / f"provider_agency_names.feather"
+    )
+
+
+def read_provider_names() -> pd.DataFrame:
+    return pd.read_feather(
+        ClimateDataPath.scripts / "oecd" / "cleaning_tools" / f"provider_names.feather"
+    )
+
+
+def read_recipient_names() -> pd.DataFrame:
+    return pd.read_feather(
+        ClimateDataPath.scripts / "oecd" / "cleaning_tools" / f"recipient_names.feather"
+    )
+
+
+def _add_names(data: pd.DataFrame, names: pd.DataFrame, idx: list[str]) -> pd.DataFrame:
+    # Add the names to the data
     data = data.merge(
-        crs,
-        on=[CrsSchema.PARTY_CODE, CrsSchema.AGENCY_CODE],
+        names,
+        on=idx,
         how="left",
-        suffixes=("", "_crs_names"),
+        suffixes=("", "_names"),
     )
 
     # drop any columns which contain the string "_crs_names"
-    data = data.drop(columns=[c for c in data.columns if "_crs_names" in c])
+    data = data.drop(columns=[c for c in data.columns if "_names" in c])
 
     return data
+
+
+def add_provider_agency_names(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add the provider agency names to the data. This function needs both Provider and
+    Agency codes in order to create matches.
+
+    Args:
+        data: The data to add the provider agency names to.
+        crs_year: The year of the CRS data to use to get names. Defaults to 2021.
+
+    Returns:
+        The data with the provider agency names added.
+
+    """
+    names = read_provider_agency_names()
+
+    return data.pipe(
+        _add_names, names=names, idx=[CrsSchema.PARTY_CODE, CrsSchema.AGENCY_CODE]
+    )
+
+
+def add_provider_names(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add the provider agency names to the data. This function needs Provider
+    codes in order to create matches.
+
+    Args:
+        data: The data to add the provider agency names to.
+        crs_year: The year of the CRS data to use to get names. Defaults to 2021.
+
+    Returns:
+        pd.DataFrame: The data with the provider names added.
+
+    """
+    names = read_provider_names()
+
+    return data.pipe(_add_names, names=names, idx=[CrsSchema.PARTY_CODE])
+
+
+def add_recipient_names(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add the provider agency names to the data. This function needs Recipient
+    codes in order to create matches.
+
+    Args:
+        data: The data to add the provider agency names to.
+        crs_year: The year of the CRS data to use to get names. Defaults to 2021.
+
+    Returns:
+        pd.DataFrame: The data with the provider names added.
+
+    """
+    names = read_recipient_names()
+
+    return data.pipe(_add_names, names=names, idx=[CrsSchema.RECIPIENT_CODE])
