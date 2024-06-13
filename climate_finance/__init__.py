42c86d13e6416e6431f1dd836f36420950970ace
Jorge Rivera <jorge.rivera@one.org>
2024-06-13 14:48:03 +0200

    Set correct paths

--- climate_finance/__init__.py
+++ climate_finance/__init__.py
@@ -9,11 +9,15 @@ from climate_finance.core.data import ClimateData
 def set_climate_finance_data_path(path):
     from pathlib import Path
     from climate_finance.config import ClimateDataPath
+    from oda_data import set_data_path
+    from pydeflate import set_pydeflate_path
 
     """Set the path to the _data folder."""
     global ClimateDataPath
 
     ClimateDataPath.raw_data = Path(path).resolve()
+    set_data_path(Path(path).resolve())
+    set_pydeflate_path(Path(path).resolve())
 
 
 __all__ = [
