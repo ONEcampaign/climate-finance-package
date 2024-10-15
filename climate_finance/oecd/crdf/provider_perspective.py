0fe2d2cb86739b08a560eef32ea669c0d7dd38e9
Jorge Rivera <jorge.rivera@one.org>
2024-10-15 10:57:40 +0200

    clean analysis tools

--- climate_finance/oecd/crdf/provider_perspective.py
+++ climate_finance/oecd/crdf/provider_perspective.py
@@ -1,12 +1,8 @@
 from pathlib import Path
-from tabnanny import check
 
 import pandas as pd
-from bblocks import convert_id
 
 from climate_finance.common.analysis_tools import (
-    filter_providers,
-    filter_recipients,
     get_providers_filter,
     get_recipients_filter,
     check_missing,
@@ -22,7 +18,6 @@ from climate_finance.oecd.crdf.tools import (
     get_marker_data,
     load_or_download,
 )
-
 
 FILE_PATH: Path = ClimateDataPath.raw_data / "oecd_climate_provider_perspective.parquet"
 BASE_URL: str = "https://webfs.oecd.org/climate/DonorPerspective/CRDF-DP-2012-"
